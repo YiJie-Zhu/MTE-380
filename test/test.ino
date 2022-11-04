ac20347908776013dd95ef6305afba49d5d44b3d
Evan Hum <54876464+e2hum@users.noreply.github.com>
2022-11-04 11:34:18 -0400

    init test file

--- test/test.ino
+++ test/test.ino
@@ -5,7 +5,13 @@
 // Arduino IDE only allows access to files in same level of folder 
 
 void setup(){
-    
+    rover r1 = rover();
+    r1.init(22, 24, 26, 28, 44, 46);
+    r1.setup();
+    r1.setSpeed(20);
+    r1.forward();
+    delay(2000);
+    r1.stop();
 }
 
 void loop(){
